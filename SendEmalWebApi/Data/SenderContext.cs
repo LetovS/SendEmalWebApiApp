d0411419a488111dd352e5618a3f96c95d34cb8c
Sergey Letov <letovs87@gmail.com>
2023-07-03 20:11:57 +0300

    Добавлены сущности

--- SendEmalWebApi/Data/SenderContext.cs
+++ SendEmalWebApi/Data/SenderContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SendEmalWebApi.Model;
 
 namespace SendEmalWebApi.Data
 {
     public class SenderContext : DbContext
     {
+        public DbSet<RequestModel> RequestModels { get; set; }
         public SenderContext(DbContextOptions<SenderContext> opt) : base (opt)
         {
             
---
+++ SendEmalWebApi/Model/RequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendEmalWebApi.Model
+{
+    public class RequestModel : Entity
+    {
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public string [] Recipients { get; set; }
+    }
+}
