50486b5321a5aa2033d4c535dac06a075e3f62fc
Sergey Letov <letovs87@gmail.com>
2023-07-03 19:39:27 +0300

    Добавлен контролер sender

---
+++ SendEmalWebApi/Controllers/SenderEmailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SendEmalWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SenderEmailController : ControllerBase
+    {
+
+    }
+}
