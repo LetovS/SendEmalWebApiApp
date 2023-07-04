93b21701a914a738eeeb56a86d7fa799430fc038
Sergey Letov <letovs87@gmail.com>
2023-07-04 21:50:05 +0300

    Рефакторинг сервиса рассылки

--- SendEmalWebApi/Services/EmailSenderService/EmailSenderService.cs
+++ SendEmalWebApi/Services/EmailSenderService/EmailSenderService.cs
@@ -3,21 +3,31 @@ using MimeKit;
 using MimeKit.Text;
 using MailKit.Security;
 using SendEmalWebApi.Model;
+using Microsoft.Extensions.Hosting;
 
 namespace SendEmalWebApi.Services.EmailSenderService
 {
     public class EmailSenderService : IEmailSenderService
     {
         private readonly IConfiguration _configuration;
+        private readonly string _host;
+        private readonly string _port;
+        private readonly string _user;
+        private readonly string _pass;
 
         public EmailSenderService(IConfiguration configuration)
         {
             _configuration = configuration;
+            var settings = _configuration.GetSection("SMPT_Server");
+            _host = settings.GetRequiredSection("Host").Value!;
+            _port = settings.GetRequiredSection("Port").Value!;
+            _user = settings.GetRequiredSection("UserName").Value!;
+            _pass = settings.GetRequiredSection("Password").Value!;
         }
         public  void SendEmail(ICollection<string> mails, string body, string subject)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("christine.boehm86@ethereal.email"));
+            email.From.Add(MailboxAddress.Parse(_user));
             foreach (var mail in mails)
             {
                 email.To.Add(MailboxAddress.Parse(mail));
@@ -25,20 +35,12 @@ namespace SendEmalWebApi.Services.EmailSenderService
             email.Subject = subject;
             email.Body = new TextPart(TextFormat.Text) { Text = body };
             using var smtp = new SmtpClient();
-            SmptSetSettings(smtp);
+            smtp.Connect(_host, int.Parse(_port!), SecureSocketOptions.StartTls);
+            smtp.Authenticate(_user, _pass);
             smtp.Send(email);
             smtp.Disconnect(true);
         }
 
-        private void SmptSetSettings(SmtpClient smtp)
-        {
-            var settings = _configuration.GetSection("SMPT_Server");
-            var host = settings.GetRequiredSection("Host").Value;
-            var port = settings.GetRequiredSection("Port").Value;
-            var user = settings.GetRequiredSection("UserName").Value;
-            var password = settings.GetRequiredSection("Password").Value;
-            smtp.Connect(host, int.Parse(port!), SecureSocketOptions.StartTls);
-            smtp.Authenticate(user, password);
-        }
+       
     }
 }
