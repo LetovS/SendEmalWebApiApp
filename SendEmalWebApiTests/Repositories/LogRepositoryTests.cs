c3206c6dec896be7a8631eb9405e0daa93047cf3
Letov Sergey <letovs87@gmail.com>
2023-07-06 22:21:26 +0300

    Добавлен проект тестов

---
+++ SendEmalWebApiTests/Repositories/LogRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SendEmalWebApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmalWebApi.Repositories.Tests
+{
+    [TestClass()]
+    public class LogRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SendEmalWebApiTests/Services/EmailSenderService/EmailSenderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SendEmalWebApi.Services.EmailSenderService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmalWebApi.Services.EmailSenderService.Tests
+{
+    [TestClass()]
+    public class EmailSenderServiceTests
+    {
+        [TestMethod()]
+        public void SendEmailTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
